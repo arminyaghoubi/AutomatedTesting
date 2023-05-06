39345413e8c1cc3f6321c027194af7250b50bad5
Armin.Yaghoubi <Armin.Yaghoubi@IT-Yaghoubi>
2023-05-06 19:06:01 +0430

    Add project files.

---
+++ test/IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/MyApp.Tests/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using MyApp.Controllers;
+
+namespace MyApp.Tests.Controllers;
+
+public class WeatherForecastControllerTest
+{
+    public class Get : WeatherForecastControllerTest
+    {
+        [Fact]
+        public void Should_be_temperature_between_two_number()
+        {
+            //Arrange
+            var controller = new WeatherForecastController();
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            foreach (var item in result)
+            {
+                Assert.InRange(item.TemperatureC, -20, 54);
+            }
+        }
+    }
+}
