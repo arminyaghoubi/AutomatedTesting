69595a96d6ff25f1c57158b2cea21d2241b061ca
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-05-16 17:22:15 +0330

    #3 Has been Created Test Without Fixture

--- src/MyApp/Program.cs
+++ src/MyApp/Program.cs
@@ -1,34 +1,30 @@
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        //Minimal Api
-        app.MapGet("/", () => "ArminYaghoubi1@gmail.com");
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//Minimal Api
+app.MapGet("/", () => "ArminYaghoubi1@gmail.com");
+
+app.Run();
+
+public partial class Program { }
---
+++ test/IntegrationTest/Controllers/ProgramTestWithoutFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using MyApp;
+
+namespace IntegrationTests.Controllers;
+
+public class ProgramTestWithoutFixture : IAsyncDisposable
+{
+    private readonly WebApplicationFactory<WeatherForecast> _factory;
+    private readonly HttpClient _client;
+
+    public ProgramTestWithoutFixture()
+    {
+        _factory = new WebApplicationFactory<WeatherForecast>();
+        _client = _factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Should_not_be_empty()
+    {
+        //Act
+        var response = await _client.GetStringAsync("/");
+
+        //Assert
+        Assert.NotEqual(string.Empty, response);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        _client.Dispose();
+        await _factory.DisposeAsync();
+    }
+}
